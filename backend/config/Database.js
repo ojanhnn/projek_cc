9b6086792d5718e8bc468d687c4b3b2c4e8e6679
rozan <ojanhannan32@gmail.com>
2025-06-03 14:07:47 +0700

    new

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('dbr-tcc','root','', {
+const db = new Sequelize('procc','root','', {
   host: "34.172.93.114",
   dialect: "mysql"
 });
